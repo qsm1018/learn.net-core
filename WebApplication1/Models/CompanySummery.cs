be3068b5ed9e9ced51d8572bd7c31a5a6319a6b5
qsm1018 <qsm1018@163.com>
2020-05-06 11:43:41 +0800

    first commit

---
+++ WebApplication1/Models/CompanySummery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CompanySummery
+    { 
+        public int EmployCount { get; set; }
+        public int AverageDepartmentEmployeeCout { get; set; }
+    }
+}
---
+++ WebApplication1/Services/DepartmentService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class DepartmentService:IDepartmentService  
+    {
+        private readonly List<Department> _departments = new List<Department>();
+        public DepartmentService()
+        {
+            _departments.Add(new Department
+            {
+                Id = 1,
+                Name = "HR",
+                EmployeeCount = 16,
+                Location = "Beijing"
+            });
+            _departments.Add(new Department
+            {
+                Id = 2,
+                Name = "R&D",
+                EmployeeCount = 52,
+                Location = "Shanghai"
+            });
+            _departments.Add(new Department
+            {
+                Id = 3,
+                Name = "Sale",
+                EmployeeCount = 200,
+                Location = "China"
+            });
+        }
+
+
+        public Task<IEnumerable<Department>> GetAll()
+        {
+            return Task.Run(()=>_departments.AsEnumerable());
+        }
+
+        public Task<Department> GetById(int id)
+        {
+            return Task.Run(() => _departments.FirstOrDefault(x=>x.Id ==id));
+        }
+
+        public Task<CompanySummery> GetCompanySummery()
+        {
+            return Task.Run(() =>
+            {
+                return new CompanySummery
+                {
+                    EmployCount = _departments.Sum(x => x.EmployeeCount),
+                    AverageDepartmentEmployeeCout = (int)_departments.Average(x => x.EmployeeCount)
+                };
+            });
+        }
+
+        public Task Add(Department department)
+        {
+            department.Id = _departments.Max(x => x.Id) + 1;
+            _departments.Add(department);
+            return Task.CompletedTask;
+        }
+
+    }
+}
